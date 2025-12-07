28fc1523175cb43ba290ac361d2d5f1295dea8e0
Simon Howard <fraggle@soulsphere.org>
2025-12-07 00:20:09 -0500

    Delete some long-obsolete scripts
    The *history scripts I used to use for updating ChangeLog files, but I
    no longer generate those files for my projects. `branch_helper` I used
    for managing Subversion branches, but I no longer use it.

--- tools/shutup_key_bell.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# 
-# Change the pitch/duration of the keyboard bell to something less annoying.
-#
-
-gconftool-2  -t int -s /desktop/gnome/peripherals/keyboard/bell_pitch 100
-gconftool-2  -t int -s /desktop/gnome/peripherals/keyboard/bell_duration 20
-
